94640c925487332f44d3b3aae71a944328568653
Tatiana Tylosky <tatianatylosky@gmail.com>
2014-08-06 20:59:23 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style type="text/css">
+      html { height: 100% }
+      body { height: 100%; margin: 0; padding: 0 }
+      #map-canvas { height: 300px; width:300px; position: relative;}
+    </style>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBR1JehK5sDvxV5rOWAUxdALF9MSBBOOZU"></script>
+    <script type="text/javascript" src="main.js"></script>
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <div id="map-canvas"></div>
+
+    <div class="ip_address">
+      <?php echo "hello world!"; ?>
+    </div>
+  </body>
+</html>
